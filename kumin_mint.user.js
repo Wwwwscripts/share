4f32184bdc65eb37147b9d0fbcbeca6be82eaf23
Wwww <krizto.as@icloud.com>
2025-10-17 01:20:41 +0100

    Update kumin_mint.user.js

--- kumin_mint.user.js
+++ kumin_mint.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kumin Mint
 // @author       GOAT
-// @version      1.0
+// @version      1.1
 // @updateURL    https://raw.githubusercontent.com/Wwwwscripts/share/main/kumin_mint.meta.js
 // @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/share/main/kumin_mint.user.js
 // @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
